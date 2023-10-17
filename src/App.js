493bc8a2d8a412472f7350acc9ce2d3bae32cf02
prakharsinghal311 <prakhargd31121999@gmail.com>
2023-10-17 18:39:23 +0530

    Maked page responsive and more dynamic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import UpperSection3 from "./assets/UpperSection3.jpg";
+import ContactUs from "./assets/Contact us.jpg";
 import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
 import GroupsIcon from "@mui/icons-material/Groups";
 import LocalPhoneOutlinedIcon from "@mui/icons-material/LocalPhoneOutlined";
@@ -7,13 +8,20 @@ import QuestionMarkOutlinedIcon from "@mui/icons-material/QuestionMarkOutlined";
 function App() {
   return (
     <>
+      <nav className="bg-[#5E17EB] h-36 grid justify-items-center">
+        <div className="mt-14 font-bold text-4xl text-white">Contact Us</div>
+        <div className="self-end font-semibold text-xl text-slate-200">
+          Have questions? We're here to help!
+        </div>
+      </nav>
+
       <img
         classNmae="h-auto max-w-full"
-        src={UpperSection3}
+        src={ContactUs}
         alt="img description"
       />
-      <div className="flex flex-row mx-32">
-        <div className="mx-12 mt-20 grid justify-items-center">
+      <div className="lg:flex lg:flex-row mx-32">
+        <div className="lg:mx-6 xl:mx-12 mt-20 grid justify-items-center">
           <div className=" h-10 w-10 flex items-center grid justify-items-center  border rounded-full bg-[#5E17EB]">
             <ChatBubbleIcon style={{ color: "white" }} />
           </div>
@@ -22,7 +30,7 @@ function App() {
             Chat live with one of our support specialists.
           </div>
         </div>
-        <div className="mx-12 mt-20 grid justify-items-center">
+        <div className="lg:mx-6 xl:mx-12 mt-20 grid justify-items-center">
           <div className=" h-10 w-10 flex items-center grid justify-items-center  border rounded-full bg-[#5E17EB]">
             <GroupsIcon style={{ color: "white" }} />
           </div>
@@ -31,7 +39,7 @@ function App() {
             Explore our community forums and communicate with other users.
           </div>
         </div>
-        <div className="mx-12 mt-20 grid justify-items-center">
+        <div className="lg:mx-6 xl:mx-12 mt-20 grid justify-items-center">
           <div className=" h-10 w-10 flex items-center grid justify-items-center  border rounded-full bg-[#5E17EB]">
             <QuestionMarkOutlinedIcon style={{ color: "white" }} />
           </div>
@@ -40,7 +48,7 @@ function App() {
             Browse FAQ's and support articles to find solutions.
           </div>
         </div>
-        <div className="mx-12 mt-20 grid justify-items-center">
+        <div className="lg:mx-6 xl:mx-12 mt-20 grid justify-items-center">
           <div className=" h-10 w-10 flex items-center grid justify-items-center  border rounded-full bg-[#5E17EB]">
             <LocalPhoneOutlinedIcon style={{ color: "white" }} />
           </div>
